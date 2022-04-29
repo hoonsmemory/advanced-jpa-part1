88c1e40e45c3661681b50a956203d944320c358b
hoon <dltjdgnsok1@naver.com>
2022-04-29 21:07:53 +0900

    주문 도메인 개발

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -68,7 +68,7 @@ public class OrderService {
      * 취소
      */
     @Transactional
-    public void CancelOrder(Long orderId) {
+    public void cancelOrder(Long orderId) {
         //주문 엔티티 조회
         Order order = orderRepository.findOne(orderId);
 
--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -4,10 +4,10 @@ import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.exception.NotEnoughStockException;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.OrderRepository;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
@@ -34,30 +34,15 @@ public class OrderServiceTest {
     public void 상품주문() throws Exception
     {
         // given
-        Member member = new Member();
-        member.setName("이성훈");
-        member.setAddress(new Address("서울","테헤란로","12345"));
-        em.persist(member);
-
-        Item item = new Book();
-        item.setName("수학책");
-        item.setPrice(10000);
-        item.setStockQuantity(100);
-        em.persist(item);
+        Member member = createMember();
+        Item item = createItem("수학책", 10000, 100);
 
         // when
         int count = 10;
         Long orderId = orderService.order(member.getId(), item.getId(), count);
 
-        System.out.println("####################################################");
-        System.out.println(item.getStockQuantity());
-        System.out.println("####################################################");
         // then
         Order getOrder = orderRepository.findOne(orderId);
-
-        System.out.println("####################################################");
-        System.out.println(item.getStockQuantity());
-        System.out.println("####################################################");
 
         assertEquals("주문 수량만큼 재고가 줄어야 한다.", 100 - count, item.getStockQuantity());
         assertEquals("상품 주문 시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
@@ -65,26 +50,59 @@ public class OrderServiceTest {
         assertEquals("주문 가격은 가격 * 수량이다.", 10000 * count, getOrder.getTotalPrice());
 
     }
-    
+
+
+
+    @Test(expected = NotEnoughStockException.class)
+    @DisplayName("상품주문_재고수량초과")
+    public void 상품주문_재고수량초과() throws Exception
+    {
+        // given
+        Member member = createMember();
+        Item item = createItem("수학책", 10000, 100);
+
+        // when
+        int count = 1000;
+        Long orderId = orderService.order(member.getId(), item.getId(), count);
+
+        // then
+        fail("재고 수량 부족 예외가 발생해야 한다.");
+    }
+
     @Test
     @DisplayName("주문 취소")
     public void 주문취소() throws Exception
     {
         // given
-        
+        Member member = createMember();
+        Item item = createItem("인프런 JPA 강의", 10000, 10);
+        int orderCount = 2;
+
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
         // when
-        
+        orderService.cancelOrder(orderId);
+
         // then
+        Order order = orderRepository.findOne(orderId);
+        assertEquals("주문 취소시 상태는 CANCEL", OrderStatus.CANCEL,order.getStatus());
+        assertEquals("주문 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
     }
-    
-    @Test
-    @DisplayName("상품주문_재고수량초과")
-    public void 상품주문_재고수량초과() throws Exception
-    {
-        // given
-        
-        // when
-        
-        // then
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("이성훈");
+        member.setAddress(new Address("서울", "테헤란로", "12345"));
+        em.persist(member);
+        return member;
     }
+
+    private Item createItem(String name, int price, int stockQuantity) {
+        Item item = new Book();
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+        em.persist(item);
+        return item;
+    }
+
 }
