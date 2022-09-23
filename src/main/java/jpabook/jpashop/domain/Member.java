7988fa3abe9bcaeed926c8cd3cf92d672678a224
leesh1993 <dltjdgnsok1@naver.com>
2022-09-23 15:45:15 +0900

    @JsonIgnore 적용

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,14 +1,14 @@
 package jpabook.jpashop.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
 @Getter @Setter
+@Entity
 public class Member {
 
     @Id @GeneratedValue
@@ -22,6 +22,18 @@ public class Member {
 
     //연관관계의 주인이 아닐 경우 mappedBy 입력
     //값을 넣어 직접 수정 불가
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
+
+    public Member() {
+
+    }
+
+    @Builder
+    public Member(String name, Address address, List<Order> orders) {
+        this.name = name;
+        this.address = address;
+        this.orders = orders;
+    }
 }
